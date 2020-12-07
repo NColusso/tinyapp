29aae54f436c4b704e0ed8ec05b29869f13b078b
Natasha Colusso <natashacolusso@gmail.com>
2020-12-07 23:24:08 +0000

    updating express server to include searching for specific short URL

--- express_server.js
+++ express_server.js
@@ -18,13 +18,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)

4ae6ed4a9a05a485d2fe2bec39d973631568f335
Natasha Colusso <natashacolusso@gmail.com>
2020-12-08 22:30:12 +0000

    updating to include a delete button to remove existing shortURLs

--- express_server.js
+++ express_server.js
@@ -38,16 +38,23 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  let newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = res.req.body.longURL;
-  res.redirect(`/urls/${newShortURL}`);
-});
-
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
   });
+
+  app.post("/urls/:shortURL/delete", (req, res) => {
+    const shortURL = req.params.shortURL
+    delete urlDatabase[shortURL];
+    res.redirect("/urls");
+  });
+  
+  app.post("/urls", (req, res) => {
+    const newShortURL = generateRandomString();
+    urlDatabase[newShortURL] = res.req.body.longURL;
+    res.redirect(`/urls/${newShortURL}`);
+  });
+  
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

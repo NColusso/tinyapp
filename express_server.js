fdaed683ee2b518dfba15d87d2fe95e4ec2940e3
Natasha Colusso <natashacolusso@gmail.com>
2020-12-11 01:27:49 +0000

    minor updates to server functions including comments

--- express_server.js
+++ express_server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-// helpers:
+// helper functions:
 const {generateRandomString} = require('./helpers.js');
 const {getUsername} = require('./helpers.js');
 const {checkForUsername} = require('./helpers.js');
@@ -40,7 +40,12 @@ const users = {
 };
 
 app.get("/", (req, res) => {
-  res.redirect("urls");
+  // redirect to either main URL page or login page based on whether or not they are logged in
+  if (req.session["user_id"]) {
+    res.redirect("urls");
+  } else {
+    res.redirect("login")
+  }
 });
 
 app.get("/urls", (req, res) => {
@@ -49,6 +54,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  // taken to page to create new URL - if not logged in taken to login page
   if (req.session["user_id"]) {
     const templateVars = { users, username: getUsername(req.session["user_id"], users) };
     res.render("urls_new", templateVars);
@@ -62,6 +68,7 @@ app.get("/urls/:shortURL", (req, res) => {
   if (!urlDatabase[shortURL]) {
     return res.status(404).send("Tiny URL does not exist");
   }
+  // check they user is both logged in and owns the specific URL
   if (req.session["user_id"] && req.session["user_id"] === urlDatabase[shortURL]["userID"]) {
     const templateVars = { users, username: getUsername(req.session["user_id"], users), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]["longURL"] };
     res.render("urls_show", templateVars);
@@ -71,13 +78,23 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]["longURL"];
-  res.redirect(longURL);
+  const shortURL = req.params.shortURL;
+  if (urlDatabase[shortURL]) {
+    const longURL = urlDatabase[req.params.shortURL]["longURL"];
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("Tiny URL does not exist");
+  }
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { users, username: getUsername(req.session["user_id"], users), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("register", templateVars);
+  // if try to register while logged in, taken to main URL page. Otherwise registration page
+  if (req.session["user_id"] in users) {
+    res.redirect("/urls");
+  } else {
+    const templateVars = { users, username: getUsername(req.session["user_id"], users), shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("register", templateVars);
+  }
 });
 
 app.get("/login", (req, res) => {
@@ -87,6 +104,7 @@ app.get("/login", (req, res) => {
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
+  // will only delete if they are logged in and their login matches owner of shortURL
   if (req.session["user_id"] && req.session["user_id"] === urlDatabase[shortURL]["userID"]) {
     delete urlDatabase[shortURL];
     res.redirect("/urls");
@@ -109,8 +127,8 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  let email = res.req.body.email;
-  let password = req.body.password;
+  const email = res.req.body.email;
+  const password = req.body.password;
   // check if there is an ID for given email and if so that passwords match
   if (getUserByEmail(email, users) && bcrypt.compareSync(password, users[getUserByEmail(email, users)].password)) {
     req.session["user_id"] = getUserByEmail(email, users);
@@ -134,9 +152,8 @@ app.post("/register", (req, res) => {
     return res.status(400).send("Error with username - either user exists or no email entered.");
   } else {
     const id = generateRandomString();
-    // create object for new user info to be added to users
+    // create object for new user info to be added to users, then add to user object
     let newUserInfo = {id, email, password: hashedPassword};
-    // add new user to user object
     users[id] = newUserInfo;
     // set cookie with new user ID
     req.session["user_id"] = id;
